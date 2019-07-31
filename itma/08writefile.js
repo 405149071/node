98bea2e9e65e317437ea95eb431ea1798f5836dc
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-07-31 16:40:20 +0800

    同步异步写入文件，小文件

--- itma/08writefile.js
+++ itma/08writefile.js
@@ -2,8 +2,14 @@ const path = require("path")
 const fs = require("fs")
 
 let filePath = path.join(__dirname, "a.txt")
+// 异步写入
 fs.writeFile(filePath, "你好啊，我是xx", function (err) {
     if (err) {
         console.log(err)
+        return
     }
+    console.log("正常写入")
 })
+
+// 同步写入
+fs.writeFileSync(filePath, "你好啊。哦哦哦")
