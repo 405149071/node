84299fd12f32cf8b97a02deb9bdfd2c973fd2d57
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-02 08:33:49 +0800

    express项目搭建

---
+++ expresscurd/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+// 用static中间件
+app.use(express.static(path.join(__dirname, "public")))
+
+// 动态请求
+app.get("/user/list", (req, res) => {
+    res.send("abc")
+    res.end()
+})
+
+app.listen(59999, function () {
+    console.log("http://127.0.0.1:59999")
+})
