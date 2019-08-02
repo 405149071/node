075fdc3cd213f9a59ac663477f589cb5e45a8709
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-02 09:38:30 +0800

    art-template基础使用

--- expresscurd/app.js
+++ expresscurd/app.js
@@ -1,15 +1,25 @@
 const express = require("express")
 const path = require("path")
+const art_express = require("express-art-template")
+
 
 const app = express()
+// 设置art-template
+app.engine("art", art_express);
 
 // 用static中间件
 app.use(express.static(path.join(__dirname, "public")))
 
 // 动态请求
 app.get("/user/list", (req, res) => {
-    res.send("abc")
-    res.end()
+    // 原始数据输出方法
+    // res.send("abc")
+    // res.end()
+    // 用模版引擎输出 基础数据
+    res.render("userlist.art", {
+        name: "zhangsan",
+        title: "你好啊！"
+    })
 })
 
 console.log("aaaabb");
--- expresscurd/readm.md
+++ expresscurd/readm.md
@@ -1,7 +1,19 @@
-# nodemon 来进行自动重启app应用
+## nodemon 来进行自动重启app应用
 ``` shell
 $ npm i -g nodemon
 
 # 使用
 nodemon app.js
 ```
+
+## art-template
+1. 引入
+```shell
+npm install -P art-template
+npm install -P express-art-template
+```
+2.项目中设置express的应用
+art-template模版引擎
+
+
+   
