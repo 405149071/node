641e2666baaf718a8580e601120557ba6304acba
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-10 14:14:25 +0800

    安装body-parse

--- koa2/app.js
+++ koa2/app.js
@@ -2,54 +2,25 @@
 var Koa = require("koa"),
     router = require("koa-router")(),
     views = require("koa-views")
-// ejs 模版引擎
-// 1. npm install koa-views --save
-// 2. npm install ejs --save
-// 3. var views  = require("koa-views")
-// app.use(views(__dirname,{extension:"ejs"}))
-// 4. await ctx.render("index")
-//  ctx.state ={ //放在中间件
-//     session:this.session,
-//     title:'app'
-// }
 
 var app = new Koa()
-
-// 配置第三方中间件 模版引擎
-// app.use(views('views', {
-//     // 第一种必须是.html结尾
-//     map: {
-//         html: "ejs"
-//     }
-// }))
 
 app.use(views("views", {
     extension: "ejs"
 }))
 
-// 写一个中间件来配置公共的信息
-app.use(async (ctx, next) => {
-    ctx.state.userinfo = "章三奥" // 也可以对象
-    await next(); //继续向下匹配
-})
-
 router.get("/", async (ctx) => {
-    // ctx.body = "首页"
-    await ctx.render("index", {
-        title: "你好ejs"
-    })
+    ctx.body = "首页"
 })
 
 router.get("/news", async (ctx) => {
-
     // ctx.body = "这是一个新闻页面"
-    let content = "<h2>绑定数据2</he>"
-    let num = 123;
-    await ctx.render("news", {
-        list: ["1111", "2222", "3333"],
-        content: content,
-        num: num,
-    })
+    await ctx.render("user")
+})
+
+router.post("/doAdd", async (ctx) => {
+    // 获取post表单数据
+
 })
 
 // 动态路由
